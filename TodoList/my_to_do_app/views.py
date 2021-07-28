ca643ecbf7718442aa319bdbdccbd96d698149b8
m0nd2y <ldj6192@gmail.com>
2021-07-28 16:40:10 +0900

    'commit'

--- TodoList/my_to_do_app/views.py
+++ TodoList/my_to_do_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 # Create your views here.
 def index(request):
