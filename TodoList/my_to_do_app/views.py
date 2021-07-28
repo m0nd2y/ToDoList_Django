05d68b5933cead5bf698787458816cdaa5acbe24
m0nd2y <ldj6192@gmail.com>
2021-07-28 16:36:19 +0900

    'commit'

--- TodoList/my_to_do_app/views.py
+++ TodoList/my_to_do_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'my_to_do_app/index.html')
     #return HttpResponse("my_to_do_app first page")
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    return HttpResponse("Create to do " + user_input_str)
