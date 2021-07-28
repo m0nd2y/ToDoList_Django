e9b3cf9794f599f9bf53b4d1a3b610965208c3bf
m0nd2y <ldj6192@gmail.com>
2021-07-28 15:50:39 +0900

    fisrt_init\

---
+++ TodoList/my_to_do_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyToDoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_to_do_app'
---
+++ TodoList/my_to_do_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'my_to_do_app/index.html')
+    #return HttpResponse("my_to_do_app first page")
