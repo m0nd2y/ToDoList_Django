ad7c322347aa4aa62e1c3a63a228c005b73e2d96
m0nd2y <ldj6192@gmail.com>
2021-08-01 18:34:49 +0900

    'commit'

--- TodoList/my_to_do_app/views.py
+++ TodoList/my_to_do_app/views.py
@@ -20,5 +20,6 @@ def doneTodo(request):
     done_todo_id = request.GET['todoNum']
     print("완료한 todo의 id", done_todo_id)
     todo = Todo.objects.get(id = done_todo_id)
-    todo.delete()
+    todo.isDone = True
+    todo.save()
     return HttpResponseRedirect(reverse('index'))
